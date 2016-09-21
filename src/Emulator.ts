cdf7c455feaf3a73f9a6bdef577f0ec1a9f6080e
Burak <burak@burak.fr>
2016-09-21 16:14:27 +0200

    Update Emulator class and config file

--- src/Emulator.ts
+++ src/Emulator.ts
@@ -15,72 +15,221 @@ import GameServer from './Networking/GameServer/GameServer';
 import GameEnvironment from './HabboHotel/GameEnvironment';
 
 export default class Emulator {
-	public static logo: string = "\r _______   __                                   _____   ______   __ \n/       \\ /  |                                 /     | /      \\ /  |\n$$$$$$$  |$$ |  ______    ______   _______     $$$$$ |/$$$$$$  |$$ |\n$$ |__$$ |$$ | /      \\  /      \\ /       \\       $$ |$$ \\__$$/ $$ |\n$$    $$< $$ |/$$$$$$  |/$$$$$$  |$$$$$$$  | __   $$ |$$      \\ $$ |\n$$$$$$$  |$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |/  |  $$ | $$$$$$  |$$/ \n$$ |__$$ |$$ |$$ \\__$$ |$$ \\__$$ |$$ |  $$ |$$ \\__$$ |/  \\__$$ | __ \n$$    $$/ $$ |$$    $$/ $$    $$/ $$ |  $$ |$$    $$/ $$    $$/ /  |\n$$$$$$$/  $$/  $$$$$$/   $$$$$$/  $$/   $$/  $$$$$$/   $$$$$$/  $$/ \n";
-	public static version: string = 'Version: 0.0.1';
+	/**
+	 * Major version of the emulator.
+	 */
+	public static MAJOR: number = 1;
+
+	/**
+	 * Minor version of the emulator.
+	 */
+	public static MINOR: number = 0;
+
+	/**
+	 * Stable build version of the emulator.
+	 */
+	public static BUILD: number = 8;
+
+	/**
+	 * Version as string.
+	 */
+	public static version: string = "Version: " + Emulator.MAJOR + "." + Emulator.MINOR + "." + Emulator.BUILD;
+
+	/**
+	 * The emulator is fully loaded and ready to handle players.
+	 */
 	public static isReady: boolean = false;
+
+	/**
+	 * The emulator is shutting down.
+	 */
+	public static isShuttingDown: boolean = false;
+
+	/**
+	 * The emulator has fully shutdown.
+	 */
 	public static stopped: boolean = false;
+
+	/**
+	 * The emulator is in debugging mode.
+	 */
 	public static debugging: boolean = true;
 
-	private static threading: ThreadPooling;
+	private static timeStarted: number = 0;
 	private static config: ConfigurationManager;
 	private static texts: TextsManager;
+	private static gameServer: GameServer;
+	//TODO: RCONServer, CameraClient
 	private static database: Database;
 	private static logging: Logging;
-	private static random: Random;
-	private static gameServer: GameServer;
+	private static threading: ThreadPooling;
 	private static gameEnvironment: GameEnvironment;
+	//TODO: PluginManager
+	private static random: Random;
+	//TODO: BadgeImager
 
+	/**
+	 * Entry point for BloonJS Emulator.
+	 * @throws Exception Failed to start the emulator.
+	 */
 	public static main(): void {
-		let startTime : number = Emulator.getCurrentTimeMillis();
+		try {
+			let startTime : number = Emulator.getCurrentTimeMillis();
 
-		Emulator.stopped = false;
-		Emulator.logging = new Logging();
-		Emulator.logging.logStart(Emulator.logo);
-		Emulator.random = new Random();
-		Emulator.threading = new ThreadPooling();
-		Emulator.config = new ConfigurationManager('../config.ini');
-		Emulator.database = new Database(Emulator.getConfig());
-		Emulator.config.loaded = true;
-		Emulator.config.loadFromDatabase();
-		Emulator.texts = new TextsManager();
-		new CleanerThread();
-		Emulator.gameServer = new GameServer(Emulator.getConfig().getValue('game.host', '127.0.0.1'), Emulator.getConfig().getInt('game.port', 30000));
-		Emulator.gameEnvironment = new GameEnvironment();
-		Emulator.gameEnvironment.load();
-		Emulator.gameServer.initialise();
-		Emulator.gameServer.connect();
+			Emulator.stopped = false;
+			Emulator.logging = new Logging();
+			Emulator.logging.logStart("\r" + Emulator.logo);
+			Emulator.random = new Random();
+			Emulator.threading = new ThreadPooling();
+			Emulator.config = new ConfigurationManager('../config.ini');
+			Emulator.database = new Database(Emulator.getConfig());
+			Emulator.config.loaded = true;
+			Emulator.config.loadFromDatabase();
+			//TODO: PluginManager
+			Emulator.texts = new TextsManager();
+			new CleanerThread();
+			Emulator.gameServer = new GameServer(Emulator.getConfig().getValue('game.host', '127.0.0.1'), Emulator.getConfig().getInt('game.port', 30000));
+			//TODO: RCONServer
+			Emulator.gameEnvironment = new GameEnvironment();
+			Emulator.gameEnvironment.load();
+			Emulator.gameServer.initialise();
+			Emulator.gameServer.connect();
+			//TODO: BadgeImager, CameraClient
+			Emulator.getLogging().logStart('Habbo Hotel Emulator has succesfully loaded.');
+			Emulator.getLogging().logStart("You're running: " + Emulator.version);
+			Emulator.getLogging().logStart("System launched in: " + (Emulator.getCurrentTimeMillis() - startTime)+ "ms!");
 
-		Emulator.getLogging().logStart('Habbo Hotel Emulator has succesfully loaded.');
-		Emulator.getLogging().logStart("You're running: " + Emulator.version);
-		Emulator.getLogging().logStart("System launched in: " + (Emulator.getCurrentTimeMillis() - startTime)+ "ms!");
+			Emulator.debugging = Emulator.getConfig().getBoolean("debug.mode");
+
+			if (Emulator.debugging) {
+				//Emulator.getLogging().logDebugLine("Debugging Enabled!");
+			}
+
+			//Emulator.getPluginManager().fireEvent(new EmulatorLoadedEvent());
+			Emulator.isReady = true;
+			Emulator.timeStarted = Emulator.getIntUnixTimestamp();
+		} catch(e) {
+			console.error(e);
+		}
 	}
 
-	public static getLogging(): Logging {
-		return this.logging;
+	private static dispose(): void {
+		Emulator.isShuttingDown = true;
+		Emulator.isReady = false;
+		//Emulator.getLogging().logShutdownLine("Stopping BloonJS Emulator " + Emulator.version + "...");
+
+		//if (Emulator.getPluginManager() != null)
+		//	Emulator.getPluginManager().fireEvent(new EmulatorStartShutdownEvent());
+
+		//if (Emulator.cameraClient != null)
+		//	Emulator.cameraClient.disconnect();
+
+		//if (Emulator.gameEnvironment != null)
+		//	Emulator.gameEnvironment.dispose();
+
+		//if (Emulator.rconServer != null)
+		//	Emulator.rconServer.stop();
+
+		//if (Emulator.gameServer != null)
+		//	Emulator.gameServer.stop();
+
+		//if (Emulator.threading != null)
+		//	Emulator.threading.shutDown();
+
+		//if (Emulator.getPluginManager() != null)
+		//	Emulator.getPluginManager().fireEvent(new EmulatorStoppedEvent());
+
+		//if (Emulator.pluginManager != null)
+		//	Emulator.pluginManager.dispose();
+
+
+		//Emulator.getLogging().logShutdownLine("Stopped BloonJS Emulator " + Emulator.version + "...");
+		Emulator.stopped = true;
 	}
 
+	/**
+	 * @return The ConfigurationManager
+	 */
 	public static getConfig(): ConfigurationManager {
 		return this.config;
 	}
 
-	public static getDatabase(): Database {
-		return this.database;
-	}
-
+	/**
+	 * @return The TextsManager
+	 */
 	public static getTexts(): TextsManager {
 		return this.texts;
 	}
 
+	/**
+	 * @return The Database
+	 */
+	public static getDatabase(): Database {
+		return this.database;
+	}
+
+	/**
+	 * @return The GameServer
+	 */
+	public static getGameServer(): GameServer {
+		return this.gameServer;
+	}
+
+	/**
+	 * @return Logging module
+	 */
+	public static getLogging(): Logging {
+		return this.logging;
+	}
+
+	/**
+	 * @return The ThreadPooling
+	 */
 	public static getThreading(): ThreadPooling {
 		return this.threading;
 	}
 
+	/**
+	 * @return The GameEnvironment
+	 */
 	public static getGameEnvironment(): GameEnvironment {
 		return this.gameEnvironment;
 	}
 
-	public static getGameServer(): GameServer {
-		return this.gameServer;
+	//TODO: PluginManager, BadgeImager, Camera
+
+	public static getTimeStarted(): number {
+		return Emulator.timeStarted;
+	}
+
+	public static prepareShutdown(): void {
+		process.exit(0);
+	}
+
+	/**
+	 * Converts a date to an unix timestamp
+	 * @param date The date to convert
+	 * @return Return unix timestamp in seconds.
+	 */
+	public static dateToUnixTimestamp(date: Date): string {
+		return Math.floor(date.getTime() / 1000 | 0).toString();
+	}
+
+	/**
+	 * Converts a String to a Date object
+	 * @param date The String to parse
+	 * @return The Date of the string.
+	 */
+	public static stringToDate(date: string): Date {
+		let res: Date = null;
+
+		try {
+			res = new Date(date);
+		} catch(e) {
+			Emulator.getLogging().logErrorLine(e);
+		}
+
+		return res;
 	}
 
 	public static dateToTimeStamp(date: Date): number {
@@ -102,6 +251,25 @@ export default class Emulator {
 	public static getCurrentTimeMillis(): number {
 		return Date.now();
 	}
+
+	public static isNumeric(n: string): boolean {
+		return !isNaN(parseFloat(n)) && isFinite(parseFloat(n));
+	}
+
+	public static validString(s: string): boolean {
+		return s.match("[^a-zA-Z0-9]") == null ? true : false;
+	}
+
+	public static logo: string =
+		" _______   __                                   _____   ______   __ \n" +
+		"/       \\ /  |                                 /     | /      \\ /  |\n" +
+		"$$$$$$$  |$$ |  ______    ______   _______     $$$$$ |/$$$$$$  |$$ |\n" +
+		"$$ |__$$ |$$ | /      \\  /      \\ /       \\       $$ |$$ \\__$$/ $$ |\n" +
+		"$$    $$< $$ |/$$$$$$  |/$$$$$$  |$$$$$$$  | __   $$ |$$      \\ $$ |\n" +
+		"$$$$$$$  |$$ |$$ |  $$ |$$ |  $$ |$$ |  $$ |/  |  $$ | $$$$$$  |$$/ \n" +
+		"$$ |__$$ |$$ |$$ \\__$$ |$$ \\__$$ |$$ |  $$ |$$ \\__$$ |/  \\__$$ | __ \n" +
+		"$$    $$/ $$ |$$    $$/ $$    $$/ $$ |  $$ |$$    $$/ $$    $$/ /  |\n" +
+		"$$$$$$$/  $$/  $$$$$$/   $$$$$$/  $$/   $$/  $$$$$$/   $$$$$$/  $$/ \n";
 }
 
 Emulator.main();
