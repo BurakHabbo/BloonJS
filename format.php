8d3cddcaadd5560dd17ce3a2817d3578a4507112
Burak <burak@burak.fr>
2016-09-28 15:11:32 +0200

    Added formatter

---
+++ format.php
@@ -0,0 +1,12 @@
+<?php
+function rglob($pattern, $flags = 0) {
+    $files = glob($pattern, $flags); 
+    foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+        $files = array_merge($files, rglob($dir.'/'.basename($pattern), $flags));
+    }
+    return $files;
+}
+
+foreach(rglob("src/*.ts") as $fileName){
+	system("tsfmt -r ".$fileName);
+}
